2d85520a439c78eaae3202f8fba11c9e9b397103
Matthew Chan <matt@themattchan.com>
2015-03-30 09:06:12 -0700

    much fibonacci

--- 06/Fibonacci.hs
+++ 06/Fibonacci.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# OPTIONS_GHC -fno-warn-missing-methods #-}
+{-@ LIQUID "--no-termination" @-}
 module Fibonacci where
 
 -- Exercise 1
@@ -32,11 +35,48 @@ streamMap f (Stream e s) = Stream (f e) (streamMap f s)
 streamFromSeed :: (a -> a) -> a -> Stream a
 streamFromSeed f s = Stream s $ streamFromSeed f (f s)
 
+streamZipWith :: (a -> b -> c) -> Stream a -> Stream b -> Stream c
+streamZipWith f (Stream a as) (Stream b bs) =
+  Stream (f a b) $ streamZipWith f as bs
+
 -- Exercise 5
 nats :: Stream Integer
-nats = go 0
-  where go n = Stream n $ go (succ n)
+nats = streamFromSeed (+1) 0
 
 interleaveStreams :: Stream a -> Stream a -> Stream a
-interleaveStreams (Stream e1 s1) (Stream e2 s2) =
-  Stream e1 $ Stream e2 $ interleaveStreams s1 s2
+interleaveStreams (Stream e1 s1) s2 = Stream e1 $ interleaveStreams s2 s1
+
+ruler :: Stream Integer
+ruler = go 0
+  where go n = interleaveStreams (streamRepeat n) (go $ succ n)
+
+-- Exercise 6
+x :: Stream Integer
+x = Stream 0 . Stream 1 $ streamRepeat 0
+
+instance Num (Stream Integer) where
+  fromInteger = flip Stream (streamRepeat 0)
+  negate      = streamMap negate
+  (+)         = streamZipWith (+)
+  (*) (Stream a as') bs@(Stream b bs') =
+    Stream (a*b) $ (+) (streamMap (*a) bs') (as' * bs)
+
+instance Fractional (Stream Integer) where
+  (/) (Stream a as) (Stream b bs) = q
+    where q = Stream (a `div` b) $ streamMap (`div` b) (as - q*bs)
+
+fibs3 :: Stream Integer
+fibs3 = x / (1 - x - x^2)
+
+-- Exercise 7
+newtype Matrix = Matrix (Integer, Integer, Integer, Integer)
+
+instance Num Matrix where
+  (*) (Matrix (a,b,c,d)) (Matrix (e,f,g,h)) =
+    Matrix (a*e+b*g, a*f+b*h, c*e+d*g, c*f+d*h)
+
+fib4 :: Integer -> Integer
+fib4 0 = 0
+fib4 n = snd $ f1^n
+  where f1 = Matrix (1,1,1,0)
+        snd (Matrix (_,b,_,_)) = b
