978975bef1c76a009d8f7ac8eb52c8eac4ff150a
Matthew Chan <matt@themattchan.com>
2015-03-29 22:26:30 -0700

    calc

---
+++ 05/Calc.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances, OverlappingInstances #-}
+module Calc where
+import ExprT
+import Parser
+import Control.Monad
+
+-- Exercise 1
+eval :: ExprT -> Integer
+eval (Lit a)     = a
+eval (Add e1 e2) = eval e1 + eval e2
+eval (Mul e1 e2) = eval e1 * eval e2
+
+-- Exercise 2
+evalStr :: String -> Maybe Integer
+evalStr s = parseExp Lit Add Mul s >>= return . eval
+
+-- Exercise 3
+class Expr a where
+  lit :: Integer -> a
+  add :: a -> a -> a
+  mul :: a -> a -> a
+
+instance Expr ExprT where
+  lit = Lit
+  add = Add
+  mul = Mul
+
+reify :: ExprT -> ExprT
+reify = id
+
+-- Exercise 4
+instance Expr Integer where
+  lit = id
+  add = (+)
+  mul = (*)
+
+instance Expr Bool where
+  lit = (> 0)
+  add = (||)
+  mul = (&&)
+
+newtype MinMax a = MinMax a deriving (Show, Eq)
+newtype Mod7   a = Mod7   a deriving (Show, Eq)
+
+instance Monad MinMax where
+  return         = MinMax
+  MinMax a >>= f = f a
+
+instance Monad Mod7 where
+  return       = Mod7
+  Mod7 a >>= f = f a
+
+instance Expr (MinMax Integer) where
+  lit = MinMax
+  add = liftM2 max
+  mul = liftM2 min
+
+instance Expr (Mod7 Integer) where
+  lit     = return . (`mod` 7)
+  add a b = liftM2 (+) a b >>= lit
+  mul a b = liftM2 (*) a b >>= lit
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger = testExp :: Maybe Integer
+testBool    = testExp :: Maybe Bool
+testMM      = testExp :: Maybe (MinMax Integer)
+testSat     = testExp :: Maybe (Mod7 Integer)
