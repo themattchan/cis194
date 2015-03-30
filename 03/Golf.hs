0fb8aaa8581747a127a5b5eb9aa1e28cb7361aee
Matthew Chan <matt@themattchan.com>
2015-03-29 21:07:01 -0700

    type signatures

--- 03/Golf.hs
+++ 03/Golf.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances, OverlappingInstances #-}
 module Golf where
 import Data.List
 
@@ -13,21 +14,21 @@ localMaxima xs = map (!!1) . filter maxs . take ((length xs) - 2) $ subs xs
         maxs (x:y:z:_) = y > x && y > z
 
 histogram :: [Integer] -> String
-histogram = (++ fmt) . concatMap toStr . reverse . rows . assoc
+histogram = (++ fmt) . concatMap show . reverse . rows . assoc
   where fmt = "==========\n0123456789\n"
 
-type Assoc a = [(a, Int)]
+type Assoc = [(Integer, Int)]
 
-assoc :: (Ord a) => [a] -> Assoc a
+assoc :: [Integer] -> Assoc
 assoc = map (\x -> (head x, length x)) . group . sort
 
-rows :: Assoc a -> [Assoc a]
+rows :: Assoc -> [Assoc]
 rows xs = if all ((==0) . snd) xs then []
           else xs : rows (map (\(n,c) -> (n, if c > 0 then c-1 else c)) xs)
 
-toStr :: Assoc Integer -> String
-toStr xs = (++"\n") $ concatMap f [0..9]
-  where f x = case lookup x xs of
-                Just 0  -> " "
-                Just _  -> "*"
-                Nothing -> " "
+instance Show Assoc where
+  show xs = (++"\n") $ concatMap f [0..9]
+    where f x = case lookup x xs of
+                  Just 0  -> " "
+                  Just _  -> "*"
+                  Nothing -> " "
